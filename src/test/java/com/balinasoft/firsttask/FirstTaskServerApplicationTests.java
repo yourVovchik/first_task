fe0a47e069952b317237a0af4690dc80a415fb1e
Nick Savenia <nikolawan@gmail.com>
2017-01-21 11:31:12 +0300

    init

---
+++ src/test/java/com/balinasoft/firsttask/FirstTaskServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.balinasoft.firsttask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstTaskServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
