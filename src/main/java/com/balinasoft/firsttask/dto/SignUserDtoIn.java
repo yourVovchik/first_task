9ae44c50db9954a24e951f2349a39ee22ab80417
Nick Savenia <nikolawan@gmail.com>
2020-10-28 14:37:34 +0300

    Fix

--- src/main/java/com/balinasoft/firsttask/dto/SignUserDtoIn.java
+++ src/main/java/com/balinasoft/firsttask/dto/SignUserDtoIn.java
@@ -2,6 +2,7 @@ package com.balinasoft.firsttask.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -10,6 +11,7 @@ import javax.validation.constraints.Size;
 
 @Setter
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SignUserDtoIn {
     @NotBlank
